13464ed5bc3313ceb676b81a91a140f4f043b058
hidekingerz <exthirix@gmail.com>
2020-08-17 01:05:12 +0900

    short codding

--- frontend/src/component/MenuAppBar.js
+++ frontend/src/component/MenuAppBar.js
@@ -23,39 +23,26 @@ const useStyles = makeStyles((theme) => ({
 const MenuAppBar = (props) => {
     const classes = useStyles();
 
-    if (props.isAuth) {
-        return (
-            <div className={classes.root}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" className={classes.title}>
-                            {props.title}
-                        </Typography>
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        {props.title}
+                    </Typography>
+                    {props.isAuth && (
                         <Button color="inherit">Logout</Button>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
-    } else {
-        return (
-            <div className={classes.root}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" className={classes.title}>
-                            {props.title}
-                        </Typography>
+                    )}
+                    {!props.isAuth && (
                         <Button color="inherit">Login</Button>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
-    }
+                    )}
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
 
 MenuAppBar.prototype = {
